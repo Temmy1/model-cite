2f939badaa25925577ec06cf4220a6a68950aac8
Artyom Ryzhkov <temmyriz@gmail.com>
2019-03-13 16:30:36 +0300

    made new sections

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.feedback').slick({
+      autoplay: false,
+      
+    //  appendArrows: $(".slide-nav"),
+        // prevArrow: $(".prev"),
+        // nextArrow: $(".next")
+    });
+  });

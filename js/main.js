1cd2d01ab5d52d38f40e1f82f82e814e81817330
Artyom Ryzhkov <temmyriz@gmail.com>
2019-03-18 17:13:04 +0300

    try to make masonry on blog page-failed swith to blog-detail

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
-$(document).ready(function(){
-    $('.feedback').slick({
-      autoplay: false,
-      
-    //  appendArrows: $(".slide-nav"),
-        // prevArrow: $(".prev"),
-        // nextArrow: $(".next")
-    });
-  });
+(function(){
+//   function resizeGridItem(item){
+//     grid = document.getElementsByClassName("other-blogs")[0];
+//     rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+//     rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+//     rowSpan = Math.ceil((item.querySelector('figure').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+//     item.style.gridRowEnd = "span "+rowSpan;
+//  }
+ 
+//  function resizeAllGridItems(){
+//     allItems = document.getElementsByClassName("blog-item");
+//     for(x=0;x<allItems.length;x++){
+//        resizeGridItem(allItems[x]);
+//     }
+//  }
+ 
+//  window.onload = resizeAllGridItems();
+ 
+//  window.addEventListener("resize", resizeAllGridItems);
+ 
+//  allItems = document.getElementsByClassName("blog-item");
+//  for(x=0;x<allItems.length;x++){
+//     imagesLoaded( allItems[x], resizeInstance);
+//  }
+ 
+//  function resizeInstance(instance){
+//     item = instance.elements[0];
+//     resizeGridItem(item);
+//  }
+
+
+})();
